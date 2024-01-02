79bc8f3f252282eeab96c2ce55a17460ee18d06a
rsenthilr <rsenthilraj83@gmail.com>
2024-01-02 18:00:13 +0530

    Android Manager - changes to capability to handle mobile apps

--- src/main/java/com/core/automation/driver/manager/mobile/local/AndroidManager.java
+++ src/main/java/com/core/automation/driver/manager/mobile/local/AndroidManager.java
@@ -8,21 +8,31 @@ import com.core.automation.config.factory.ConfigFactory;
 import io.appium.java_client.android.AndroidDriver;
 
 public class AndroidManager {
-	
+
 	private AndroidManager() {
 	}
-	
+
 	public static WebDriver getDriver() {
-		
+
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		//capabilities.setCapability("app", System.getProperty("user.dir")+"/src/test/resources/Android_APKFiles/android-app.apk");
+		// -- Common capability
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("automationName", "UiAutomator2");
-		capabilities.setCapability("browserName", "chrome");
-		capabilities.setCapability("chromedriverExecutableDir", System.getProperty("user.dir")+"/CoreAutomation/src/test/resources/MobileDrivers/");
-		capabilities.setCapability("chromedriverChromeMappingFile", System.getProperty("user.dir")+"/CoreAutomation/src/test/resources/MobileDrivers/mapping.json");
-		capabilities.setCapability("chromedriver_autodownload", "--allow-insecure");
-		return new AndroidDriver(ConfigFactory.getConfig().LocalAppiumServerURL(), capabilities);	
+		
+		// -- App installation capability
+		capabilities.setCapability("app", System.getProperty("user.dir")
+				+ "/src/test/resources/Android_APKFiles/android-app.apk");
+		
+		// -- Mobile Browser Capability
+		//capabilities.setCapability("browserName", "chrome");
+		// capabilities.setCapability("chromedriverExecutableDir",
+		// System.getProperty("user.dir")+"/CoreAutomation/src/test/resources/MobileDrivers/");
+		// capabilities.setCapability("chromedriverChromeMappingFile",
+		// System.getProperty("user.dir")+"/CoreAutomation/src/test/resources/MobileDrivers/mapping.json");
+		// capabilities.setCapability("chromedriver_autodownload",
+		// "--allow-insecure");
+		return new AndroidDriver(
+				ConfigFactory.getConfig().LocalAppiumServerURL(), capabilities);
 	}
 
 }
