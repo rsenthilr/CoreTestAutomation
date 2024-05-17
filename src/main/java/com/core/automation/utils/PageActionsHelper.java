511cc0a1dc1c22a357f47ce8041cf8a3b0ec15da
a822846 <a822846@host.docker.internal>
2024-05-17 18:56:24 +0530

    Changes done to PageActions Helper class

--- src/main/java/com/core/automation/utils/PageActionsHelper.java
+++ src/main/java/com/core/automation/utils/PageActionsHelper.java
@@ -8,23 +8,26 @@ import com.core.automation.driver.factory.DriverManager;
 import com.core.automation.driver.factory.ExplicitWaitFactory;
 
 public class PageActionsHelper {
-	
+
 	private PageActionsHelper() {
 	}
-	
+
+	public static String getPageTitle() {
+
+		return DriverManager.getDriver().getTitle();
+	}
+
 	public static void waitAndClick(By by, WaitStrategy waitStrategy) {
-		WebElement element = ExplicitWaitFactory.performExplicitWait(by, waitStrategy);
+		WebElement element = ExplicitWaitFactory.performExplicitWait(by,
+				waitStrategy);
 		element.click();
 	}
-	
-	public static void waitAndSendKeys(By by, String value, WaitStrategy waitStrategy) {
-		WebElement element = ExplicitWaitFactory.performExplicitWait(by, waitStrategy);
+
+	public static void waitAndSendKeys(By by, String value,
+			WaitStrategy waitStrategy) {
+		WebElement element = ExplicitWaitFactory.performExplicitWait(by,
+				waitStrategy);
 		element.sendKeys(value);
-	}
-	
-	public static String getPageTitle() {
-		
-		return DriverManager.getDriver().getTitle();
 	}
 
 }
